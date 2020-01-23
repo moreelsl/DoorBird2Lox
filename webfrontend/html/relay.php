b5f39305396ea920f1d94f7f5df0f3d15cc35afd
Luk <60239744+moreelsl@users.noreply.github.com>
2020-01-23 23:58:30 +0100

    condense code

--- webfrontend/html/relay.php
+++ webfrontend/html/relay.php
@@ -2,17 +2,11 @@
 
 require_once "loxberry_io.php";
  
-$response = msudp_send(1, 10001, "DoorBirdHTTP2UDP", $_GET["name"]);
+$response = msudp_send(1, 10001, "DoorBird2Lox", $_GET["name"]);
 if (!isset($response)) {
     echo "Error sending to Miniserver";
 } else {
-    echo "Sent ok.";
+    echo "Sent ok. Relay: ";
+    echo htmlspecialchars($_GET["name"]); 
 }
-
-echo htmlspecialchars($_GET["name"]); 
-
-
-
-?> 
-
-
+?>
