7d54fcfb2dd2edfee22c36ac18b56ae8f771c74a
Luk <60239744+moreelsl@users.noreply.github.com>
2020-01-23 23:25:03 +0100

    ^.

--- webfrontend/htmlauth/index.php
+++ webfrontend/htmlauth/index.php
@@ -1,8 +1,10 @@
 <html>
  <head>
-  <title>PHP-Test</title>
+  <title>PHP-Test Config</title>
  </head>
  <body>
+ This plugin should offer an alternative to the DoorBird IP I/O Door Controller A1081 device.
+
  <?php echo '<p>Hallo Welt</p>'; ?>
  </body>
 </html>
