a9d26e58ed3f81e3a5669a811e5e0e08c0c986f8
Luk <60239744+moreelsl@users.noreply.github.com>
2020-01-31 12:24:56 +0100

    .

--- webfrontend/htmlauth/index.php
+++ webfrontend/htmlauth/index.php
@@ -1,6 +1,7 @@
 <?php
 require_once "loxberry_web.php";
 require_once "loxberry_system.php";
+require_once "Config/Lite.php";
   
 // This will read your language files to the array $L
 $L = LBSystem::readlanguage("language.ini");
@@ -8,9 +9,13 @@ $L = LBSystem::readlanguage("language.ini");
 // Getting data from current plugin 
 $plugin = LBSystem::plugindata();
 
+// Open config file
+$cfg = new Config_Lite("$lbpconfigdir/pluginconfig.cfg",LOCK_EX,INI_SCANNER_RAW);
+
+// Header
 $template_title = $plugin['PLUGINDB_TITLE'];
-$helplink = "http://www.loxwiki.eu:80/x/2wzL";
-$helptemplate = "help.html";
+$helplink       = "http://www.loxwiki.eu:80/x/2wzL";
+$helptemplate   = "help.html";
   
 LBWeb::lbheader($template_title, $helplink, $helptemplate);
  
@@ -20,6 +25,9 @@ LBWeb::lbheader($template_title, $helplink, $helptemplate);
 
 <p><?=$L['BASIC.WELCOMEMESSAGE']?></p>
 <p><?=$L['BASIC.HEADING_BASICSETTINGS']?></p>
+
+
+<p><?=$cfg['SECTION1']['NAME'];?></p>
  
 <?php 
 // Finally print the footer 
