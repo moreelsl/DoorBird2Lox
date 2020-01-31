9d08fd0a1d7267505faa513cd2619790a6fd9548
Luk <60239744+moreelsl@users.noreply.github.com>
2020-01-31 11:51:30 +0100

    .

--- webfrontend/htmlauth/index.php
+++ webfrontend/htmlauth/index.php
@@ -3,8 +3,37 @@
   <title>PHP-Test Config</title>
  </head>
  <body>
- This plugin should offer an alternative to the DoorBird IP I/O Door Controller A1081 device.
+
 
  <?php echo '<p>Hallo Welt</p>'; ?>
  </body>
 </html>
+
+
+<?php
+require_once "loxberry_web.php";
+require_once "loxberry_system.php";
+  
+// This will read your language files to the array $L
+$L = LBSystem::readlanguage("language.ini");
+
+// Getting data from current plugin 
+$plugin = LBSystem::plugindata();
+
+$template_title = $plugin['PLUGINDB_TITLE'];
+$helplink = "http://www.loxwiki.eu:80/x/2wzL";
+$helptemplate = "help.html";
+  
+LBWeb::lbheader($template_title, $helplink, $helptemplate);
+ 
+// This is the main area for your plugin
+?>
+
+<p>This plugin should offer an alternative to the DoorBird IP I/O Door Controller A1081 device.</p>
+<p><?=$L['SOMEAREA.WELCOMEMESSAGE']?></p>
+<p><?=$L['SOMEAREA.HOWTOMESSAGE']?></p>
+ 
+<?php 
+// Finally print the footer 
+LBWeb::lbfooter();
+?>
